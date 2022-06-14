3c74b28f51fa83e550a15ec31fa74806ab8d70f0
Maxwell Marte <davidmarte345@gmail.com>
2022-06-14 01:22:51 -0400

    Build 0.3 - Tienda atravez de prompts, panel de actualizaciones

--- main.js
+++ main.js
@@ -76,8 +76,77 @@ while (registerConfirm) {
       );
     }
 
-    alert(`Realiza su tu pedido`);
-    console.log(`Proximamente con la version con facturacion`);
+    alert(`Realiza  tu pedido`);
+    let total = 0;
+    let producto;
+    let cantidad;
+    let seguirComprando;
+    let ag = "Agregaste:";
+
+    do {
+      producto = prompt(
+        "Coloca el codigo del producto deseado \n Codigo |    Producto    | Precio:\n  10001  |  Agua 16oz   | 50$  \n  10002  | Pasta dental  | 75$  \n  10003  |  Cepillos D   | 25$  \n  10004  |  Leche 16oz  | 80$  \n  10005  | Carne Fresca | 200$ \n  10006  | Carne Frizada| 100$ \n  10007  | Pimienta x Lb| 25$  \n  10008  |     Ajo x Lb     | 15$  "
+      );
+      cantidad = parseInt(prompt("Cuantos quieres comprar?"));
+
+      let precio;
+
+      switch (producto) {
+        case "10001":
+          precio = 50;
+          alert(`${ag} ${cantidad} Agua 16oz`);
+          break;
+        case "10002":
+          precio = 75;
+          alert(`${ag} ${cantidad} Pasta dental`);
+          break;
+        case "10003":
+          precio = 25;
+          alert(`${ag} ${cantidad} Cepillo dental`);
+          break;
+        case "10004":
+          precio = 80;
+          alert(`${ag} ${cantidad} Leche 16oz`);
+          break;
+        case "10005":
+          precio = 200;
+          alert(`${ag} ${cantidad}LB de Carne fresca`);
+          break;
+        case "10006":
+          precio = 100;
+          alert(`${ag} ${cantidad}LB de Carne frizada`);
+          break;
+        case "10007":
+          precio = 25;
+          alert(`${ag} ${cantidad}LB de Pimienta`);
+          break;
+        case "10008":
+          precio = 15;
+          alert(`${ag} ${cantidad}LB de Ajo`);
+          break;
+
+        default:
+          alert(
+            "Este Producto no esta en el listado o no fue escrito \ncorrectamente"
+          );
+          precio = 0;
+          cantidad = 0;
+          break;
+      }
+
+      total = precio * cantidad;
+
+      seguircomprando = confirm("Quieres seguir comprando?");
+    } while (seguirComprando);
+
+    let finalizarCompra = confirm(
+      `El total de la compra es: ${total} \n \n Desea Finalizar la compra?`
+    );
+
+    if (finalizarCompra) {
+      alert("Su Compra ha sido realizado con exito en 1hr va estar con usted");
+    } else "";
+
     break;
   } else {
     alert(`Gracias por la visita te esperamos pronto`);
